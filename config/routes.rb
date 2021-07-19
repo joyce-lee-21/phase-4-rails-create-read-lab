25e4a3e17f93b39e224b48ea52e8794510d01071
Joyce Lee <joycelee901@gmail.com>
2021-07-19 15:25:37 -0500

    commit tree d54f6150425a8a08100734ee10cd7fe8e285e755

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/plants', to: 'Plants#index'
+  # get '/plants/:id', to: 'Plants#show'
+  # get '/plants', to: 'Plants#create'
+  resources :plants, only: [:index, :show, :create]
 end
